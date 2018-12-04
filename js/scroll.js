510864c12e2ada6b510ee686a446200bb658b70a
Rita Mitina <rita.mitina.00@bk.ru>
2018-12-04 18:54:27 +0300

    result

--- js/scroll.js
+++ js/scroll.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
 
 
   new fullpage('.maincontent',{
-    anchors: ['welcome__page','','burger__page','team__page','menu__page','reviews__page','player__page','map__page'],
+    anchors: ['welcome__page','','burger__page','team__page','menu__page','reviews__page','player__page','','map__page'],
     loopTop: true,
     loopBottom:true,
     navigation:true,
